fc2c05b51ef98b103d26fb663f472a18ef3319f1
傅诚 <327619852@qq.com>
2019-09-02 16:41:58 +0800

    add vue-bus

--- src/components/Achild.vue
+++ src/components/Achild.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     I am AChild
+    <button @click="increment">emit</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Achild'
+  name: 'Achild',
+  methods: {
+    increment() {
+      this.bus.$emit('increment', '我是increment')
+    }
+  },
 }
 </script>
 
--- src/components/Bchild.vue
+++ src/components/Bchild.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
     I am BChild
+    <p>{{info}}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Bchild'
+  name: 'Bchild',
+  data() {
+    return {
+      info: 'default info'
+    }
+  },
+  mounted() {
+    let _this = this
+    console.log('Bchild this', this)
+    this.bus.$on('increment', function(data) {
+      console.log('这是Bchild收到的值: ', data)
+      console.log('Bchild in _this: ', _this)
+      _this.info = data
+    })
+  },
 }
 </script>
 
