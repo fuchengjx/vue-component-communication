452ae3a43611a0db731ca72c6fe3439540e9c11b
傅诚 <327619852@qq.com>
2019-09-04 21:30:34 +0800

    console biology

--- src/components/Achild.vue
+++ src/components/Achild.vue
@@ -12,9 +12,34 @@ export default {
     increment() {
       console.log('A触发了 $emit')
       this.bus.$emit('increment', '我是increment')
+      this.$router.push('/B')
     }
   },
-}
+   beforeCreate () {
+    console.group('%c%s', 'color:red', 'beforeCreate 创建前状态===============组件A》')
+  },
+  created () {
+    console.group('%c%s', 'color:red', 'created 创建完毕状态===============组件A》')
+  },
+  beforeMount () {
+    console.group('%c%s', 'color:red', 'beforeMount 挂载前状态===============组件A》')
+  },
+  mounted () {
+    console.group('%c%s', 'color:red', 'mounted 挂载状态===============组件A》')
+  },
+  beforeUpdate () {
+    console.group('%c%s', 'color:red', 'beforeUpdate 更新前状态===============组件A》')
+  },
+  updated () {
+    console.group('%c%s', 'color:red', 'updated 更新状态===============组件A》')
+  },
+  beforeDestroy () {
+    console.group('%c%s', 'color:red', 'beforeDestroy 破前状态===============组件A》')
+  },
+  destroyed () {
+    console.group('%c%s', 'color:red', 'destroyed 破坏状态===============组件A》')
+  }
+ }
 </script>
 
 <style>
--- src/components/Bchild.vue
+++ src/components/Bchild.vue
@@ -13,15 +13,39 @@ export default {
       info: 'default info'
     }
   },
-  mounted() {
-    let _this = this
-    console.log('Bchild this', this)
-    this.bus.$on('increment', function(data) {
-      console.log('这是Bchild收到的值: ', data)
-      console.log('Bchild in _this: ', _this)
-      _this.info = data
-    })
+  // mounted() {
+  //   let _this = this
+  //   console.log('Bchild this', this)
+  //   this.bus.$on('increment', function(data) {
+  //     console.log('这是Bchild收到的值: ', data)
+  //     console.log('Bchild in _this: ', _this)
+  //     _this.info = data
+  //   })
+  // },
+   beforeCreate () {
+    console.group('%c%s', 'color:blue', 'beforeCreate 创建前状态===============组件B》')
   },
+  created () {
+    console.group('%c%s', 'color:blue', 'created 创建完毕状态===============组件B》')
+  },
+  beforeMount () {
+    console.group('%c%s', 'color:blue', 'beforeMount 挂载前状态===============组件B》')
+  },
+  mounted () {
+    console.group('%c%s', 'color:blue', 'mounted 挂载状态===============组件B》')
+  },
+  beforeUpdate () {
+    console.group('%c%s', 'color:blue', 'beforeUpdate 更新前状态===============组件B》')
+  },
+  updated () {
+    console.group('%c%s', 'color:blue', 'updated 更新状态===============组件B》')
+  },
+  beforeDestroy () {
+    console.group('%c%s', 'color:blue', 'beforeDestroy 破前状态===============组件B》')
+  },
+  destroyed () {
+    console.group('%c%s', 'color:blue', 'destroyed 破坏状态===============组件B》')
+  }
 }
 </script>
 
